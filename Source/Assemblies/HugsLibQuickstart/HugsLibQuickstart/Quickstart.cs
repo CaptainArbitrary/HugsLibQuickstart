7d43f8aba0ec4b30ea87f4747bced239f322cd2d
Jeffery Harrell <jefferyharrell@gmail.com>
2023-09-02 12:46:27 -0700

    Add Quickstart class (Verse.Mod subclass)
    - Added Quickstart.cs file with Quickstart class
    - Quickstart class inherits from Mod and has a private field _settings of type QuickStartSettings
    - Constructor initializes _settings by calling GetSettings<QuickStartSettings>()

---
+++ Source/Assemblies/HugsLibQuickstart/HugsLibQuickstart/Quickstart.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+
+namespace HugsLibQuickstart
+{
+    public class Quickstart : Mod
+    {
+        private readonly QuickStartSettings _settings;
+
+        public Quickstart(ModContentPack content) : base(content)
+        {
+            _settings = GetSettings<QuickStartSettings>();
+        }
+    }
+}
