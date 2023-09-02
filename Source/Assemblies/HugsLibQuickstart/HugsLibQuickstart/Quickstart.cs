f178a07f38a0a14cc8d2cc1de998fae4525f8d41
Jeffery Harrell <jefferyharrell@gmail.com>
2023-09-02 16:28:07 -0700

    Add basic implementation of automatic map generation
    - Includes automatic generation at startup of a 250 × 250 map using the "Crashlanded" default scenario
    - Generation can be aborted by hitting the space bar
    - Game pauses on load

--- Source/Assemblies/HugsLibQuickstart/HugsLibQuickstart/Quickstart.cs
+++ Source/Assemblies/HugsLibQuickstart/HugsLibQuickstart/Quickstart.cs
@@ -1,14 +1,144 @@
-﻿using Verse;
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+using Verse.Profile;
 
-namespace HugsLibQuickstart
+namespace HugsLibQuickstart;
+
+public class Quickstart : Mod
 {
-    public class Quickstart : Mod
-    {
-        private readonly QuickStartSettings _settings;
+    private bool _quickstartPending;
+    private QuickstartSettings _settings;
+    private QuickstartStatusBox _statusBox;
 
-        public Quickstart(ModContentPack content) : base(content)
+    public Quickstart(ModContentPack content) : base(content)
+    {
+        if (Settings.OperationMode == QuickstartSettings.QuickstartMode.Disabled) return;
+
+        Log.Message($"Settings.OperationMode = {Settings.OperationMode}");
+
+        Harmony harmony = new(content.PackageId);
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+        _quickstartPending = true;
+        _statusBox = new QuickstartStatusBox(GetStatusBoxOperation(Settings));
+        _statusBox.AbortRequested += StatusBoxAbortRequestedHandler;
+
+        QuickstartStatusBox.IOperationMessageProvider GetStatusBoxOperation(QuickstartSettings settings)
         {
-            _settings = GetSettings<QuickStartSettings>();
+            return settings.OperationMode switch
+            {
+                QuickstartSettings.QuickstartMode.LoadMap => new QuickstartStatusBox.LoadSaveOperation(
+                    GetSaveNameToLoad() ?? string.Empty),
+                QuickstartSettings.QuickstartMode.GenerateMap => new QuickstartStatusBox.GenerateMapOperation(
+                    settings.ScenarioToGen, settings.MapSizeToGen),
+                _ => throw new ArgumentOutOfRangeException("Unhandled operation mode: " + settings.OperationMode)
+            };
         }
+    }
+
+    public QuickstartSettings Settings
+    {
+        get { return _settings ??= GetSettings<QuickstartSettings>(); }
+    }
+
+    private string GetSaveNameToLoad()
+    {
+        return Settings.SaveFileToLoad ?? TryGetMostRecentSaveFileName();
+    }
+
+    private string TryGetMostRecentSaveFileName()
+    {
+        string mostRecentFilePath = GenFilePaths.AllSavedGameFiles.FirstOrDefault()?.Name;
+        return Path.GetFileNameWithoutExtension(mostRecentFilePath);
+    }
+
+    private void StatusBoxAbortRequestedHandler(bool abortAndDisable)
+    {
+        _quickstartPending = false;
+        Log.Warning("Quickstart aborted: Space key was pressed.");
+        if (abortAndDisable)
+        {
+            Settings.OperationMode = QuickstartSettings.QuickstartMode.Disabled;
+            LongEventHandler.ExecuteWhenFinished(WriteSettings);
+        }
+    }
+
+    public void OnGUIUnfiltered()
+    {
+        if (!_quickstartPending) return;
+        _statusBox.OnGUI();
+    }
+
+    internal void OnLateInitialize()
+    {
+        // RetrofitSettingWithLabel();
+        // EnumerateMapSizes();
+        if (Prefs.DevMode) LongEventHandler.QueueLongEvent(InitiateQuickstart, null, false, null);
+    }
+
+    private void InitiateQuickstart()
+    {
+        if (!_quickstartPending) return;
+        _quickstartPending = false;
+        _statusBox = null;
+
+        if (Settings.OperationMode == QuickstartSettings.QuickstartMode.Disabled) return;
+
+        CheckForErrorsAndWarnings();
+
+        if (Settings.OperationMode == QuickstartSettings.QuickstartMode.GenerateMap) InitiateMapGeneration();
+        // } else if (Settings.OperationMode == QuickstartSettings.QuickstartMode.LoadMap)
+        // {
+        // InitiateSaveLoading();
+    }
+
+    private void CheckForErrorsAndWarnings()
+    {
+        if (Settings.StopOnErrors && Log.Messages.Any(m => m.type == LogMessageType.Error)) throw new WarningException("errors detected in log");
+        if (Settings.StopOnWarnings && Log.Messages.Any(m => m.type == LogMessageType.Warning)) throw new WarningException("warnings detected in log");
+    }
+
+    private void InitiateMapGeneration()
+    {
+        Log.Message("Quickstarter generating map with scenario: " + GetMapGenerationScenario().name);
+        LongEventHandler.QueueLongEvent(() =>
+        {
+            MemoryUtility.ClearAllMapsAndWorld();
+            ApplyQuickstartConfiguration();
+            PageUtility.InitGameStart();
+            Find.TickManager.Pause();
+        }, "GeneratingMap", true, GameAndMapInitExceptionHandlers.ErrorWhileGeneratingMap);
+    }
+
+    private Scenario GetMapGenerationScenario()
+    {
+        // return TryGetScenarioByName(Settings.ScenarioToGen) ?? ScenarioDefOf.Crashlanded.scenario;
+        return ScenarioDefOf.Crashlanded.scenario;
+    }
+
+    private void ApplyQuickstartConfiguration()
+    {
+        Current.ProgramState = ProgramState.Entry;
+        Current.Game = new Game
+        {
+            InitData = new GameInitData(),
+            Scenario = GetMapGenerationScenario()
+        };
+        Find.Scenario.PreConfigure();
+        Current.Game.storyteller = new Storyteller(StorytellerDefOf.Cassandra, DifficultyDefOf.Rough);
+        Current.Game.World = WorldGenerator.GenerateWorld(0.05f, GenText.RandomSeedString(),
+            OverallRainfall.Normal, OverallTemperature.Normal, OverallPopulation.Normal);
+        Find.GameInitData.ChooseRandomStartingTile();
+        Find.GameInitData.mapSize = Settings.MapSizeToGen;
+        Find.Scenario.PostIdeoChosen();
+        Find.GameInitData.PrepForMapGen();
+        Find.Scenario.PreMapGenerate();
     }
 }
---
+++ Source/Assemblies/HugsLibQuickstart/HugsLibQuickstart/QuickstartStatusBox.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace HugsLibQuickstart;
+
+internal class QuickstartStatusBox
+{
+    public delegate void AbortHandler(bool abortAndDisable);
+
+    private static readonly Vector2 StatusRectSize = new(240f, 75f);
+    private static readonly Vector2 StatusRectPadding = new(26f, 18f);
+
+    private readonly IOperationMessageProvider _pendingOperation;
+
+    public QuickstartStatusBox(IOperationMessageProvider pendingOperation)
+    {
+        this._pendingOperation = pendingOperation ?? throw new ArgumentNullException(nameof(pendingOperation));
+    }
+
+    public static bool ShiftIsHeld => Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+
+    public event AbortHandler AbortRequested;
+
+    public void OnGUI()
+    {
+        string statusText = GetStatusBoxText();
+        Rect boxRect = GetStatusBoxRect(statusText);
+        DrawStatusBox(boxRect, statusText);
+        HandleKeyPressEvents();
+    }
+
+    private string GetStatusBoxText()
+    {
+        StringBuilder sb = new StringBuilder("HugsLib quickstarter preparing to\n");
+        sb.Append(_pendingOperation.Message);
+        sb.AppendLine();
+        sb.AppendLine();
+        sb.Append("<color=#777777>");
+        sb.AppendLine("Press Space to abort");
+        sb.Append("Press Shift+Space to disable");
+        sb.Append("</color>");
+        return sb.ToString();
+    }
+
+    private static Rect GetStatusBoxRect(string statusText)
+    {
+        Vector2 statusTextSize = Text.CalcSize(statusText);
+        float boxWidth = Mathf.Max(StatusRectSize.x, statusTextSize.x + StatusRectPadding.x * 2f);
+        float boxHeight = Mathf.Max(StatusRectSize.y, statusTextSize.y + StatusRectPadding.y * 2f);
+        Rect boxRect = new Rect(
+            (UI.screenWidth - boxWidth) / 2f,
+            (UI.screenHeight / 2f - boxHeight) / 2f,
+            boxWidth, boxHeight
+        );
+        boxRect = boxRect.Rounded();
+        return boxRect;
+    }
+
+    private static void DrawStatusBox(Rect rect, string statusText)
+    {
+        Widgets.DrawShadowAround(rect);
+        Widgets.DrawWindowBackground(rect);
+        TextAnchor prevAnchor = Text.Anchor;
+        Text.Anchor = TextAnchor.MiddleCenter;
+        Widgets.Label(rect, statusText);
+        Text.Anchor = prevAnchor;
+    }
+
+    private void HandleKeyPressEvents()
+    {
+        if (Event.current.type == EventType.KeyDown && Event.current.keyCode == KeyCode.Space)
+        {
+            bool abortAndDisable = ShiftIsHeld;
+            Event.current.Use();
+            AbortRequested?.Invoke(abortAndDisable);
+        }
+    }
+
+    public interface IOperationMessageProvider
+    {
+        string Message { get; }
+    }
+
+    public class LoadSaveOperation : IOperationMessageProvider
+    {
+        private readonly string _fileName;
+
+        public LoadSaveOperation(string fileName)
+        {
+            this._fileName = fileName;
+        }
+
+        public string Message => $"load save file: {_fileName}";
+    }
+
+    public class GenerateMapOperation : IOperationMessageProvider
+    {
+        private readonly int _mapSize;
+        private readonly string _scenario;
+
+        public GenerateMapOperation(string scenario, int mapSize)
+        {
+            this._scenario = scenario;
+            this._mapSize = mapSize;
+        }
+
+        public string Message => $"generate map: {_scenario} ({_mapSize}x{_mapSize})";
+    }
+}
