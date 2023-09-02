427ee691b32702005fba4fe71805990611fff641
Jeffery Harrell <jefferyharrell@gmail.com>
2023-09-02 12:46:55 -0700

    Add QuickStartSettings class (Verse.ModSettings subclass)
    - Added QuickStartSettings.cs file to the project.
    - Implemented QuickStartSettings class with properties and methods.
    - Added enum QuickstartMode for different operation modes.
    - Exposed data using Scribe_Values.Look method in ExposeData() method.

---
+++ Source/Assemblies/HugsLibQuickstart/HugsLibQuickstart/QuickStartSettings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Verse;
+
+namespace HugsLibQuickstart
+{
+    public class QuickStartSettings : ModSettings
+    {
+        public enum QuickstartMode {
+            Disabled = 0,
+            LoadMap = 1,
+            GenerateMap = 2
+        }
+
+        public QuickstartMode OperationMode = QuickstartMode.Disabled;
+        public string SaveFileToLoad;
+        public string ScenarioToGen;
+        // public int MapSizeToGen = QuickstartController.DefaultMapSize;
+        public bool StopOnErrors = true;
+        public bool StopOnWarnings;
+        public bool BypassSafetyDialog;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref OperationMode, "OperationMode");
+            Scribe_Values.Look(ref SaveFileToLoad, "SaveFileToLoad");
+            Scribe_Values.Look(ref ScenarioToGen, "ScenarioToGen");
+            Scribe_Values.Look(ref StopOnErrors, "StopOnErrors");
+            Scribe_Values.Look(ref StopOnWarnings, "StopOnWarnings");
+            Scribe_Values.Look(ref BypassSafetyDialog, "BypassSafetyDialog");
+
+            base.ExposeData();
+        }
+        
+    }
+}
