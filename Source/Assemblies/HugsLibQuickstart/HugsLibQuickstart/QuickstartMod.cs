1abf2ae859c0de52d186c89206f9be740709e4ad
Jeffery Harrell <jefferyharrell@gmail.com>
2023-09-03 09:38:46 -0700

    Add SuppressMessage attribute to QuickstartMod class
    - Add [SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")] attribute to QuickstartMod class

--- Source/Assemblies/HugsLibQuickstart/HugsLibQuickstart/QuickstartMod.cs
+++ Source/Assemblies/HugsLibQuickstart/HugsLibQuickstart/QuickstartMod.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -13,6 +14,7 @@ using Verse.Profile;
 
 namespace HugsLibQuickstart;
 
+[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
 public class QuickstartMod : Mod
 {
     public static readonly List<MapSizeEntry> MapSizes = new();
